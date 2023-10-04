5890cc98819e5c0a1207c6bd140394e5a11e6460
Alex Li <ali602@gatech.edu>
2023-10-03 21:00:17 -0400

    Added more javascript code for right side

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
 let ID = 1;
 
 async function getData() {
-    // const response = await fetch(`https://pokeapi.co/api/v2/ability/${ID}/`);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`);
     const data = await response.json();
     const pic = document.getElementById("pic");
     const name = document.getElementById("name");
+    const stats = document.getElementById("stats");
 
     pic.src = data.sprites.front_default;
-    name.innerText = data.species.name;
-    /*
-    const p = document.getElementById("move");
-    p.innerText = data.name;
-    */
+    name.innerText = data.name;
+    stats.innerText = getInfo(data);
+
 }
 
 function moveLeft() {
@@ -21,7 +19,6 @@ function moveLeft() {
         ID--;
     }   
 }
-    
 
 function moveRight() {
     if(ID <= 1000) {
@@ -29,10 +26,32 @@ function moveRight() {
     }
 }
 
+function getInfo(data) {
+    return `
+        height: ${data.height / 10}m
+        weight: ${data.weight / 10}kg
+        hp: ${data.stats[0].base_stat}
+        attack: ${data.stats[1].base_stat}
+        defense: ${data.stats[2].base_stat}
+        special-attack: ${data.stats[3].base_stat}
+        special-defense: ${data.stats[4].base_stat}
+        speed: ${data.stats[5].base_stat}
+    `;
+}
+
+function getMoves(data) {
+    let moves = "";
+    for(let i = 0; i < data.length && i < 10; i++) {
+        moves += moves[i].move.name + "\n";
+    }
+    return moves;
+}
+
 const leftButton = document.getElementById("left");
 const rightButton = document.getElementById("right");
 const infoButton = document.getElementById("info");
 const moveButton = document.getElementById("move");
+
 leftButton.addEventListener("click", (event) => {
     moveLeft();
     getData();
@@ -43,5 +62,38 @@ rightButton.addEventListener("click", (event) => {
     getData();
 });
 
+infoButton.addEventListener("click", (event) => {
+    toggleInfo();
+});
+
+moveButton.addEventListener("click", (event) => {
+    toggleMove();
+});
+
+infoButton.value = "ON";
+moveButton.value = "OFF";
+
+async function toggleInfo() {
+    if(infoButton.value == "OFF") {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`);
+        const data = await response.json();
+        const stats = document.getElementById("stats");
+        infoButton.value = "ON";
+        moveButton.value = "OFF";
+        stats.innerText = getInfo(data);
+    }
+}
+
+async function toggleMove() {
+    if(moveButton.value == "OFF") {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID}`);
+        const data = await response.json();
+        const stats = document.getElementById("stats");
+        infoButton.value = "OFF";
+        moveButton.value = "ON";
+        stats.innerText = getMoves(data);
+    }
+}
+
+
 getData();
-// console.log(getData());
